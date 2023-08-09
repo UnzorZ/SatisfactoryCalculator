43f2b908da545acdb60c6d4a57cb6a5997b0c0f2
UnzorZ <73613908+UnzorZ@users.noreply.github.com>
2023-08-09 09:23:04 +0200

    Added image support

--- src/main/java/dev/unzor/Objects/Recipe.java
+++ src/main/java/dev/unzor/Objects/Recipe.java
@@ -18,22 +18,33 @@ public class Recipe {
         return product;
     }
 
+    public String getImageUrl() {
+        return image_url;
+    }
+    public void setImageUrl(String url) {
+        image_url = url;
+    }
+
     private final String name;
     private final String keyname;
     private final Map<String, Integer> ingredients;
     private final Map<String, Integer> product;
-    public Recipe(String name, String keyname, Map<String, Integer> ingredients, Map<String, Integer> product) {
+    private String image_url;
+    public Recipe(String name, String keyname, Map<String, Integer> ingredients, Map<String, Integer> product, String image_url) {
         this.name = name;
         this.keyname = keyname;
         this.ingredients = ingredients;
         this.product = product;
+        this.image_url = image_url;
     }
 
     public String toString() {
         return "Name: " + name +
                 ", Keyname: " + keyname +
                 ", Ingredients: " + ingredients +
-                ", Product: " + product;
+                ", Product: " + product +
+                ", Image: " + image_url;
+
     }
 
 
