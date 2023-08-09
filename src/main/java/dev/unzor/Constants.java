918799df9067eac563f1db6eb73d1866e3df0e43
UnzorZ <73613908+UnzorZ@users.noreply.github.com>
2023-08-09 09:26:21 +0200

    Added an error image placeholder

--- src/main/java/dev/unzor/Constants.java
+++ src/main/java/dev/unzor/Constants.java
@@ -2,4 +2,5 @@ package dev.unzor;
 
 public class Constants {
     public static boolean debug = false;
+    public static String errorImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Cross_red_circle.svg/2048px-Cross_red_circle.svg.png";
 }
